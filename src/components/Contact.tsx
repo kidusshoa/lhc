524128c56b9cdd84da972a8c00f824e1a2914a01
kidusshoa <captainkevin2008@gmail.com>
2024-09-20 15:44:50 +0300

    shadow

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -33,8 +33,8 @@ const ContactUs: React.FC = () => {
     <div id="contact" className="flex flex-col w-full h-full">
       <div className="text-[#312e2e] md:h-screen h-full w-full flex flex-col items-center justify-center">
         <div className="h-4/5 w-full flex items-center lg:flex-row flex-col md:p-10 p-3 justify-center gap-5">
-          <div className="md:w-2/4 w-full h-full shadow-lg p-5 md:p-0 rounded-3xl flex flex-col items-center justify-center gap-5">
-            <h1 className="text-3xl font-bold text-[#903839]">
+          <div className="md:w-2/4 w-full h-full shadow-lg shadow-[#903839] p-5 md:p-0 rounded-3xl flex flex-col items-center justify-center gap-5">
+            <h1 className="text-3xl font-bold  text-[#903839]">
               Get In Touch With Us
             </h1>
             <form
@@ -45,18 +45,18 @@ const ContactUs: React.FC = () => {
                 {...register("full_name", { required: true })}
                 type="text"
                 placeholder="Your Name"
-                className="rounded border-[#903839] shadow-lg transition:0.3s p-3 md:w-4/5 box-border focus:border-2 focus:border-[#903839] bg-white text-xl outline-none"
+                className="rounded border-[#903839] shadow-lg shadow-[#903839]  transition:0.3s p-3 md:w-4/5 box-border focus:border-2 focus:border-[#903839] bg-white text-xl outline-none"
               />
               <input
                 {...register("email", { required: true })}
                 type="email"
                 placeholder="Your Email"
-                className="rounded border-[#903839] shadow-lg transition:0.3s p-3 md:w-4/5 box-border focus:border-2 focus:border-[#903839] bg-white text-xl outline-none"
+                className="rounded border-[#903839] shadow-lg shadow-[#903839] transition:0.3s p-3 md:w-4/5 box-border focus:border-2 focus:border-[#903839] bg-white text-xl outline-none"
               />
               <textarea
                 {...register("comment", { required: true })}
                 placeholder="Your Message"
-                className="rounded border-[#903839] shadow-lg transition:0.3s p-3 md:w-4/5 box-border focus:border-2 focus:border-[#903839] bg-white text-xl outline-none"
+                className="rounded border-[#903839] shadow-lg shadow-[#903839] transition:0.3s p-3 md:w-4/5 box-border focus:border-2 focus:border-[#903839] bg-white text-xl outline-none"
                 rows={5}
               ></textarea>
               <input
